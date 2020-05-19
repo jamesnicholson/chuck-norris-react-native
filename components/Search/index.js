7c836ee63bce5ed5eaec06dbd89a701ebbac71a9
James Nicholson <james.n@tencent.co.za>
2020-05-19 21:10:57 +0200

    Refactored basic Search

--- components/Search/index.js
+++ components/Search/index.js
@@ -5,36 +5,37 @@ import styled from 'styled-components'
 import { SEARCH_FOR_JOKES }  from '../../apollo/queries'
 Search = () =>{
     const [searchTerm, setSearchTerm ] = useState("");
-    const [getJokes, {loading, data }] = useLazyQuery(SEARCH_FOR_JOKES, {
+    const [getJokes, {loading, data, error}] = useLazyQuery(SEARCH_FOR_JOKES, {
         fetchPolicy: "no-cache",
-        variables: { searchTerm: searchTerm },
-    })    
+        variables: { searchTerm: searchTerm }})
+
+    useEffect(()=>{
+       console.log(data)
+    },[data])
+ 
     const JokeView = styled.View`
-        color:#000000;
-      background-color: #eeeeee;`
-      const JokeText = styled.Text`
       color:#000000;
-    background-color: #eeeeee;`
+      background-color: #eeeeee;`
+
+    const JokeText = styled.Text`
+      color:#000000;
+      background-color: #eeeeee;`
+
     const JokeButton = styled.Button`
-    color:#000000;
-    background:#eeeeee;`
+      color:#000000;
+      background:#eeeeee;`
+
     if(loading) return <JokeView><JokeText>Loading</JokeText></JokeView>
 
       return <JokeView>
                 <Text>Search Page</Text>
+                <Button 
+                  onPress={()=>(getJokes())}
+                  title="Search" />
                 <TextInput
                     onChangeText={text => setSearchTerm(text)}
                     value={searchTerm}
                     />
-        
-                <JokeButton
-                    title="Categoryies"
-                    onPress={() =>getJokes()}
-                  />
-              {
-                  data === undefined ? <JokeView><JokeText>data</JokeText></JokeView> : <JokeView><JokeText>no data</JokeText></JokeView> 
-              }
-                    
             </JokeView>
 }
 
